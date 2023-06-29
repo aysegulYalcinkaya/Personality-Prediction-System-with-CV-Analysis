17516a1a1a465a30b126950f451fcbfa1f763659
aysegulYalcinkaya <aysegulyalcinkayadel@gmail.com>
2023-06-28 21:49:07 -0400

    setup.py created

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CVAnalysis',
+    version='1.0',
+    packages=['users', 'users.migrations', 'employer', 'employer.migrations', 'CVAnalysis'],
+    url='',
+    license='',
+    author='Aysegul Yalcinkaya',
+    author_email='aysegul.yalcinkaya@dcmail.ca',
+    description=''
+)
